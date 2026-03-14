e14c757d6b4591cee19a25ce26fcad752e262a9c
Khainh2005 <10423053@student.vgu.edu.vn>
2026-03-14 11:53:56 +0700

    adding structure

--- src/FTPclient.java
+++ src/FTPclient.java
@@ -1,9 +1,18 @@
+import java.util.*;
+import java.net.*;
+import java.io.*;
 
 public class FTPclient {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("haha. This is the test");
+		
+		try {
+			//Socket s = new Socket("")
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
